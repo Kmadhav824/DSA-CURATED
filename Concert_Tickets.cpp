53f7645350ab99833d4feeea5632df973379033e
Madhav <kmadhav824@gmail.com>
2025-09-13 00:10:42 +0530

    Solved Concert Ticket using multiset for log(n) erase

--- Concert_Tickets.cpp
+++ Concert_Tickets.cpp
@@ -3,24 +3,29 @@ using namespace std;
 int main()
 {
     int n,m;cin>>n>>m;
-    vector<int> v1(n);
+    multiset<int> v1;
     vector<int> v2(n);
     for(int i=0;i<n;i++)
     {
-        cin>>v1[i];
+        int x;cin>>x;
+        v1.insert(x);
     }
     for(int i=0;i<m;i++)
     {
         cin>>v2[i];
     }
-    sort(v1.begin(),v1.end());
-    // sort(v2.begin(),v2.end());
-    int ptr1=0,ptr2=0;
-    while(ptr1<n && ptr2<m)
+    for(int i=0;i<m;i++)
     {
-        if(v2[ptr2]<v1[ptr1])
+        auto it=v1.upper_bound(v2[i]);
+        if(it==v1.begin())
         {
-            ptr
+            cout<<-1<<endl;
+        }
+        else
+        {
+            it--;
+            cout<<*it<<endl;
+            v1.erase(it);
         }
     }
 }
